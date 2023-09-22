59cafd8a180cf07a40f72ce942cd63776f9035f2
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-09-22 16:21:45 -0700

    Add purgeCSS install

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit({
+			preprocess: sveltePreprocess({
+				postcss: {},
+				scss: {},
+				sass: {},
+			})
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
