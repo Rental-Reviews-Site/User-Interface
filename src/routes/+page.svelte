77a9a9b5e3e9a264fd710d4bde91b1d3d92cd487
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-09-29 16:37:06 -0700

    Add file forwarding to scss files and visually test fonts display in browser

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="ff-accent">Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
-<h2 class="fw">hey fellas</h2>
-
-// h1 and logo need to not reroute to this same page, but be static; don't want
-to trigger pointless reloads
+<h2 class="ff-base">hey fellas</h2>
