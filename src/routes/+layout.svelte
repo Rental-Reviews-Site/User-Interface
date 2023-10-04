1534e0c3ab61609e4a321fcf098bdeb082852cc4
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-10-04 16:03:11 -0700

    Fix header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,17 +2,23 @@
   import "$lib/main.scss";
 </script>
 
-<header class="site-header">
-  <div class="site-logo">
-    <a href="/">LOGO</a>
-  </div>
-  <nav class="nav">
-    <a href="/share">share</a>
-    <a href="/contact">contact</a>
-    <a href="/login">login</a>
-  </nav>
-</header>
-<slot />
-<footer class="site-footer">
-  <p>little footer</p>
-</footer>
+<div class="center">
+  <header class="site-header">
+    <div class="site-header__inner">
+      <div class="site-logo">
+        <a href="/">LOGO</a>
+      </div>
+      <nav class="nav">
+        <a href="/share">share</a>
+        <a href="/contact">contact</a>
+        <a href="/login">login</a>
+      </nav>
+    </div>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer class="site-footer">
+    <small>little footer</small>
+  </footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
-<main class="section">
-  <h1 class="ff-accent">Welcome to SvelteKit</h1>
-  <p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-  </p>
+<div class="">
+  <h1 class="ff-accent">Welcome to Rental Reviews</h1>
   <nav class="site-nav">
     <a href="search">add review</a>
     <a href="see-reviews">see reviews</a>
     <a href="filter">browse</a>
   </nav>
-  <h2 class="ff-base">hey fellas</h2>
-</main>
+  <h2 class="ff-base">hey fellas,</h2>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+</div>
